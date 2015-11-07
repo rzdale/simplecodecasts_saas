a0e12575233b253cb00e85da0bc3a1ba6b64d7ed
Rob <robzd1@gmail.com>
2015-11-07 01:11:07 +0000

    Fixed typo in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,8 +10,8 @@ class ApplicationController < ActionController::Base
   
   def configure_permitted_parameters
     
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation)
-    
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :stripe_card_token, :email, :password, :password_confirmation) }
+
   end
   
   
